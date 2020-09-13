9b79abcf0b3c023988bbfcf4066e0236435d1590
Stephen <iamstephenbellocillo@gmail.com>
2020-09-13 14:55:39 +0800

    [sure na nga access token]

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {library} from './library/library';
 
-let stIGAccessToken = "IGQVJVWUh2SXVfU0NYMEhwaGMyRkNNYUlNblMwdkhwUXJnZA1N2NjlFT08zekhpZA0JwMXo3cC1zdTc3N0xmdFZAhMm9fZAXdVOUY3QWtxYWpOVXVpSXZAqMWw3YmR1RjREanFtUlBRSzhNZAlpJV04zWGlERnVWR0xSUTBrbmNj";
+let stIGAccessToken = "IGQVJWYkFlV1VMZAXVLTGViMDEtT1duZAjRnRjM5MkJLbDZAwZADZAIeVA2bm5rWDA4cnYzVjVKR2xTYWJMSTdjZAlpNbEpoN0lQSXpNdXBKTUdNWkhjWjRvd2lrSm4yQlFyYVlWUVBHU0hrd185TjI0SEJITndVcGlMX3NYVlFF";
 let stAccountMediaId = "https://graph.instagram.com/me/media?fields=id,caption&access_token=" + stIGAccessToken;
 
 function load(){
