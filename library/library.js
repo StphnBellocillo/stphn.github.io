de102675f61211c9a12411035ef427914659c3eb
Stephen <iamstephenbellocillo@gmail.com>
2020-09-13 13:18:48 +0800

    [m]

--- library/library.js
+++ library/library.js
@@ -1,7 +1,3 @@
-export function Table(stAccountMediaId) {
-    
-}
-
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const xmlHttp = new XMLHttpRequest();
 
@@ -10,7 +6,7 @@ const library = {};
 let stIGAccessToken = "IGQVJXbGRYdGc1UzZA5LW8xRXVXbXF3Ym1zeG13VERBU29uV0FlVHZAiWDBWQjFBQnRsTGUyM0x2U2hQM3dNb1N4YjZAGbnZAwQnZArWHNHUldnT180N3R4TjJveENxMkpnSGhQMEZAfeDRMMGM1RHBLdUp1dmJlN2NFclh4MUU0";
 let stAccountMediaId = "https://graph.instagram.com/me/media?fields=id,caption&access_token=" + stIGAccessToken;
 
-function httpGetMediaId(url){
+function httpGetMediaId(url) {
     let arr = [];
     let obj = {};
     try {
@@ -24,23 +20,22 @@ function httpGetMediaId(url){
                 }
             }
         }
-    }
-    catch (e) {
-        console.log("errror:",e);
+    } catch (e) {
+        console.log("errror:", e);
     }
     console.log("httpGetMediaId:", arr);
     return arr;
 }
 
-function httpGetMediaImageUrl(url){
+function httpGetMediaImageUrl(url) {
     console.log("httpGetMediaImageUrl url", url);
     let arr = [];
     let obj = {};
     let objMediaIds = httpGetMediaId(url);
     console.log("httpGetMediaImageUrl obj:", objMediaIds);
-    for(var i in objMediaIds){
+    for (var i in objMediaIds) {
         /*        console.log("httpGetMediaImageUrl objMediaIds:", );*/
-        obj.ImageUrl = "https://graph.instagram.com/" +objMediaIds[i].id
+        obj.ImageUrl = "https://graph.instagram.com/" + objMediaIds[i].id
             + "?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJXdWZAPdHRWT2JQRDljaEpzR3pXTEhpRC1tLWFJbThPVGdXc2hHc0IxUjN0bkVaZAHJmaVhlcEpJMnhJUVFMa2hud2ZAXV0t6a0JzME5qMlBqQzlnV2szQ1Bkc3FYU3hWNS1DU1RtT25aR3otZA3Q4a2dsdVZAleGNiOE53";
         arr.push(obj);
     }
@@ -48,26 +43,25 @@ function httpGetMediaImageUrl(url){
     return arr;
 }
 
-function httpGet(theUrl)
-{
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+function httpGet(theUrl) {
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
     return JSON.parse(xmlHttp.responseText);
 }
 
-library.Table = function(url){
+library.Table = function (url) {
     let htmlString = "";
     let rowString = "";
     let count = 0;
     console.log("url", url);
     let objUrl = httpGetMediaImageUrl(url);
-    for(let i in objUrl){
+    for (let i in objUrl) {
         rowString += '<div class="col-sm">\n';
         rowString += '<img class="card-img-top" src="' + objUrl[i].ImageUrl + '">\n';
         rowString += '</div>\n';
 
         /*        console.log("count", count);*/
-        if(count == 3) {
+        if (count == 3) {
             /*         console.log("ara naku sa count", count);*/
             htmlString += '<div class="row">\n';
             htmlString += rowString;
@@ -75,7 +69,7 @@ library.Table = function(url){
             htmlString += ' <br>\n';
             htmlString += ' <br>\n';
             rowString = "";
-            count = 0 ;
+            count = 0;
         }
         count++;
         /*    console.log("count", count);*/
@@ -83,8 +77,9 @@ library.Table = function(url){
     var str = document.getElementById("ig").innerHTML;
     var res = str.replace("{imageTable}", htmlString);
     document.getElementById("ig").innerHTML = res;
-    return htmlString ;
+    return htmlString;
 
 }
 
-return library;
+
+export {library};
